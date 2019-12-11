44ffb493c0e6b3fbeb6c9b66437c0ab2d8038bda
Traineau <nathan.trn@orange.fr>
2019-12-11 14:57:54 +0100

    Final fix after merging

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,13 +1,13 @@
-import {createStackNavigator} from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import SearchPharmacistsContainer from '../containers/SearchPharmacistsContainer'
-import UserInfoContainer from '../containers/UserInfoContainer'
-import ChatPageContainer from '../containers/ChatPageContainer'
-import BlogPageContainer from '../containers/BlogPageContainer'
-import AuthPageContainer from '../containers/AuthPageContainer'
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import SearchPharmacistsContainer from "../containers/SearchPharmacistsContainer";
+import UserInfoContainer from "../containers/UserInfoContainer";
+import ChatPageContainer from "../containers/ChatPageContainer";
+import BlogPageContainer from "../containers/BlogPageContainer";
+import AuthPageContainer from "../containers/AuthPageContainer";
 import React from "react";
 import * as colors from "react-native";
-import {Text} from "react-native";
+import { Text } from "react-native";
 import RegisterPageContainer from "../containers/RegisterPageContainer";
 
 const SearchPharmacistsNavigator = createStackNavigator(
@@ -20,15 +20,15 @@ const SearchPharmacistsNavigator = createStackNavigator(
 );
 
 const UserInfoNavigator = createStackNavigator(
-    {
-        UserInfo: UserInfoContainer,
-        AuthPage:AuthPageContainer,
-        RegisterPage: RegisterPageContainer,
-    },
-    {
-      initialRouteName: 'UserInfo',
-    }
-  );
+  {
+    UserInfo: UserInfoContainer,
+    AuthPage: AuthPageContainer,
+    RegisterPage: RegisterPageContainer
+  },
+  {
+    initialRouteName: "UserInfo"
+  }
+);
 const BlogNavigator = createStackNavigator(
   {
     BlogPage: BlogPageContainer
@@ -38,87 +38,65 @@ const BlogNavigator = createStackNavigator(
   }
 );
 
-  const SearchPharmacistsNavigator = createStackNavigator(
-    {
-      SearchPharmacists: SearchPharmacistsContainer,
-    },
-    {
-      initialRouteName: 'SearchPharmacists',
-    }
-  );
-
-const TabNavigator = createBottomTabNavigator(
+const ChatNavigator = createStackNavigator(
   {
+    ChatPage: ChatPageContainer
+  },
+  {
+    initialRouteName: "ChatPage"
+  }
+);
+
+export const TabNavigator = createBottomTabNavigator(
+  {
+    Blog: {
+      screen: BlogNavigator,
+      navigationOptions: () => ({
+        tabBarIcon: () => (
+          <Text>0</Text>
+          //<FontAwesomeIcon icon="coffee" name="user" size={24} />
+        )
+      })
+    },
     SearchPharmacists: {
       screen: SearchPharmacistsNavigator,
       navigationOptions: () => ({
-        tabBarIcon: () => <FontAwesome icon={SolidIcons.smile} />
+        tabBarIcon: () => (
+          <Text>1</Text>
+          // <FontAwesomeIcon  icon="coffee" name ="search" size={24}/>
+        )
       })
     },
     Chat: {
       screen: ChatNavigator,
       navigationOptions: () => ({
-        tabBarIcon: () => <FontAwesome icon={SolidIcons.smile} />
+        tabBarIcon: () => (
+          <Text>2</Text>
+          //<FontAwesomeIcon icon="coffee" name="comment-medical" size={24} />
+        )
       })
     },
     UserInfo: {
       screen: UserInfoNavigator,
       navigationOptions: () => ({
-        tabBarIcon: () => <FontAwesome icon={SolidIcons.smile} />
+        tabBarIcon: () => (
+          <Text>3</Text>
+          //<FontAwesomeIcon icon="coffee" name="user" size={24} />
+        )
       })
     }
-  );
-
-  export const TabNavigator = createBottomTabNavigator(
-    {
-        Blog: {
-            screen: BlogNavigator,
-            navigationOptions: () => ({
-                tabBarIcon: () => (
-                    <Text>0</Text>
-                    //<FontAwesomeIcon icon="coffee" name="user" size={24} />
-                ),
-            }),
-        },
-        SearchPharmacists: {
-            screen: SearchPharmacistsNavigator,
-            navigationOptions: () => ({
-              tabBarIcon: () => (
-                  <Text>1</Text>
-                // <FontAwesomeIcon  icon="coffee" name ="search" size={24}/>
-              ),
-            }),
-        },
-        Chat: {
-            screen: ChatNavigator,
-            navigationOptions: () => ({
-              tabBarIcon: () => (
-                  <Text>2</Text>
-                //<FontAwesomeIcon icon="coffee" name="comment-medical" size={24} />
-              ),
-            }),
-        },
-        UserInfo: {
-        screen: UserInfoNavigator,
-        navigationOptions: () => ({
-          tabBarIcon: () => (
-              <Text>3</Text>
-            //<FontAwesomeIcon icon="coffee" name="user" size={24} />
-          ),
-        }),
-        },
-    },
-    {
-      tabBarOptions: {
-        showLabel: false, // hide labels
-        activeTintColor: colors.white, // active icon color
-        inactiveTintColor: colors.modal_grey_2, // inactive icon color
-        style: {
-          backgroundColor: colors.dark_grey, // TabBar background
-        },
-      },
-    },
-    {
-      initialRouteName: 'SearchPharmacists',
-    },
-  );
+  },
+  {
+    tabBarOptions: {
+      showLabel: false, // hide labels
+      activeTintColor: colors.white, // active icon color
+      inactiveTintColor: colors.modal_grey_2, // inactive icon color
+      style: {
+        backgroundColor: colors.dark_grey // TabBar background
+      }
+    }
+  },
+  {
+    initialRouteName: "SearchPharmacists"
+  }
+);

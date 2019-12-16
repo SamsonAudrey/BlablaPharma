80683738a414aa528b799454c4631bab9fc44c08
Samson Audrey <audrey.samson29@gmail.com>
2019-12-16 10:55:31 +0100

    add image  sign in

--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import CButton from "../components/Button";
 
 class Register extends Component {
@@ -12,6 +12,12 @@ class Register extends Component {
         const {navigate} = this.props.navigation;
         return (
             <View>
+                <View>
+                    <Image
+                        style={{width: '100%', height: 100}}
+                        source={require('../assets/sign-in-pharmacist.png')}
+                    />
+                </View>
                 <CButton title={"Patient"} buttonStyle={'green'} onPress={() => navigate('RegisterPage_Patient', {userKind:'patient'})}/>
                 <CButton title={"Pharmacien"} buttonStyle={'grey'} onPress={() => navigate('RegisterPage_Patient', {userKind:'pharmacist'})}/>
             </View>

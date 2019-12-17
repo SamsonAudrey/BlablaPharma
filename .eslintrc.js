baa9e7d7819dceffbf283c4c9a6d33a74324905c
Samson Audrey <audrey.samson29@gmail.com>
2019-12-17 15:15:25 +0100

    fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@
 
 module.exports = {
     root: true,
-    //extends: '@react-native-community',
     "env": {
         "react-native/react-native": true
     },
@@ -26,13 +25,20 @@ module.exports = {
         "react",
         "react-native"
     ],
-    "rules": {
-        "react-native/no-unused-styles": 2,
-        "react-native/split-platform-components": 2,
-        "react-native/no-inline-styles": 2,
-        "react-native/no-color-literals": 2,
-        "react-native/no-raw-text": 2,
-        "react-native/no-single-element-style-arrays": 2,
+    'extends': [
+        'airbnb',
+    ],
+    'parser': 'babel-eslint',
+    'rules': {
+        'no-use-before-define': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/prop-types': 'off',
+        'comma-dangle': 'off',
+        "quotes": [2, "single", "avoid-escape"],
+        'no-nested-ternary': 'off',
+    },
+    'globals': {
+        "fetch": false
     }
   };
 

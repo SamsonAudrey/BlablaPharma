157b4cdc111db25cf1e6db9194411faa12f55571
Traineau <nathan.trn@orange.fr>
2020-01-29 13:24:10 +0100

    swipe delete conv

--- src/components/chat/ConversationListItems.js
+++ src/components/chat/ConversationListItems.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, TouchableOpacity, Button
+} from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import CButton from '../buttons/Button';
 
 export default class ConversationListItems extends Component {
-  render() {
-    const conversation = this.props.conversation.item;
-    const { otherPerson, onDelete } = this.props;
-    return (
-      <>
-        <View>
+renderRightActions = () => (
+    <TouchableOpacity
+      onPress={() => this.props.onDelete(conversation.id)}
+      style={{ scaleY: 100, scaleX: 2, backgroundColor: '#cc0000' }}
+    />
+)
+
+render() {
+  const conversation = this.props.conversation.item;
+  const { otherPerson, onDelete } = this.props;
+  return (
+    <Swipeable
+      renderRightActions={this.renderRightActions}
+    >
+      <View>
+        <Text>
+          {otherPerson.firstName}
+          {' '}
+          {otherPerson.lastName}
+        </Text>
+        {conversation.messages.length > 0 ? (
           <Text>
-            {otherPerson.firstName}
-            {' '}
-            {otherPerson.lastName}
+            {conversation.messages[0].read ? 'true' : 'false'}
+            {conversation.messages[0].content}
           </Text>
-          {conversation.messages.length > 0 ? (
-            <Text>
-              {conversation.messages[0].read ? "true": "false"}
-              {conversation.messages[0].content}
-            </Text>
-          ) : <Text>Pas de message pour l instant</Text>}
-        </View>
-        <CButton
-          onPress={() => this.props.handlPress(conversation.id, otherPerson)}
-          title="Conv"
-        />
-        <CButton
-          onPress={() => onDelete(conversation.id)}
-          title="Delete"
-        />
-      </>
-    );
-  }
+        ) : <Text>Pas de message pour l instant</Text>}
+      </View>
+      <CButton
+        onPress={() => this.props.handlPress(conversation.id, otherPerson)}
+        title="Conv"
+      />
+    </Swipeable>
+  );
+}
 }

68c2047383df35f093e11faa04a169424ec8dec9
Samson Audrey <audrey.samson29@gmail.com>
2020-02-11 17:12:54 +0100

    fix

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -13,7 +13,7 @@ const AuthScreenStack = createStackNavigator({
   <View style={{ flex: 1, alignItems: 'center' }}>
     <Image
       source={require('../assets/logo-navbar.png')}
-      style={{ width: '78%', height: '100%' }}
+      style={{ width: '78%', height: Platform.OS === 'ios' ? 43 : 40 }}
     />
   </View>
     })
